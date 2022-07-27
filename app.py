f070d4f226c3cccf149dbc003df81b4f624d5cd2
Aishwarya Majumdar <aishu.maj1996@gmail.com>
2022-07-27 12:37:57 -0700

    completed app

--- app.py
+++ app.py
@@ -1,1 +1,46 @@
-# Put your app in here.
+from flask import Flask, request
+from operations import add, sub, mult, div
+
+app = Flask(__name__)
+
+@app.get("/add")
+def do_add():
+    """Adds queries a and b"""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    final = add(a,b)
+
+    return str(final)
+
+
+@app.get("/sub")
+def do_sub():
+    """Subtracts queries a and b"""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    final = sub(a,b)
+
+    return str(final)
+
+@app.get("/mult")
+def do_mult():
+    """Multiples queries a and b"""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    final = mult(a,b)
+
+    return str(final)
+
+@app.get("/div")
+def do_div():
+    """Divides queries a by b"""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    final = div(a,b)
+
+    return str(final)
+
